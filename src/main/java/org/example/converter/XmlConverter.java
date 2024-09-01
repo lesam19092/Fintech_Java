bd6c99ba1c5c7430bd4e9db9b4c6437e632cae93
lesam19092 <danigpro1337@gmail.com>
2024-09-01 21:49:56 +0300

    some refactoring fomatting and imports

--- src/main/java/org/example/converter/XmlConverter.java
+++ src/main/java/org/example/converter/XmlConverter.java
@@ -2,19 +2,8 @@ package org.example.converter;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.example.pojo.City;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
--- src/main/java/org/example/pojo/Coordinates.java
+++ src/main/java/org/example/pojo/Coordinates.java
@@ -1,6 +1,8 @@
 package org.example.pojo;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
