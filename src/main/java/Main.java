b8b4a3507a41870f17d02916eee115a6ce6ace27
lesam19092 <danigpro1337@gmail.com>
2024-09-04 23:47:51 +0300

    edited Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,30 @@
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.example.converter.XmlConverter;
 import org.example.pojo.City;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
+@Slf4j
 public class Main {
 
-    private static final Logger logger = Logger.getLogger(XmlConverter.class.getName());
-
     public static void main(String[] args) throws IOException {
-
         File file = new File("city.json");
         ObjectMapper objectMapper = new ObjectMapper();
+
+        if (!file.exists()) {
+            log.warn("File " + file.getName() + " not found. Processing might fail.");
+            return;
+        }
         try {
             City city = objectMapper.readValue(file, City.class);
-            System.out.println(city.toString());
+            log.debug("city is created successfully");
             XmlConverter.toXml(city);
         } catch (JsonParseException exception) {
-            logger.log(Level.SEVERE, exception.getMessage());
+            log.error(exception.getMessage());
         }
     }
 }
